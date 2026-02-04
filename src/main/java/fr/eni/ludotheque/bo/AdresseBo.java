3c4d1cb7410621c184e8e62294066ada45511c38
Marine CEDE <marine.cede2025@campus-eni.fr>
2026-02-04 14:42:13 +0100

    fix: modification de AdresseBo

--- src/main/java/fr/eni/ludotheque/bo/AdresseBo.java
+++ src/main/java/fr/eni/ludotheque/bo/AdresseBo.java
@@ -1,13 +1,22 @@
 package fr.eni.ludotheque.bo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class AdresseBo {
-    @Id
+    @Column
     int noAdresse;
+    @Column
     String rue;
+    @Column
     int codePostal;
+    @Column
     String ville;
 }
